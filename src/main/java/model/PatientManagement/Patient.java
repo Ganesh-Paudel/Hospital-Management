409052405f9afb8233f2886b9c205a04cbf25503
Ganesh <paudelganesh080@gmail.com>
2026-04-21 03:34:23 -0400

    Added Implementation in model for patients and Encounters.

--- src/main/java/model/PatientManagement/Patient.java
+++ src/main/java/model/PatientManagement/Patient.java
@@ -1,4 +1,51 @@
 package model.PatientManagement;
 
+import jakarta.persistence.*;
+import lombok.*;
+import model.operations.Encounter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "patients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "patient_id")
+    private Integer patientId;
+
+    @Column(name = "first_name", length = 50)
+    private String firstName;
+
+
+    @Column(name = "last_name", length = 50)
+    private String lastName;
+
+    @Column(name = "dob")
+    private String dob;
+
+    @Column(name = "gender", length = 10)
+    private String gender;
+
+    @Column(name = "phone", length = 20)
+    private String phone;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(columnDefinition = "TEXT")
+    private String address;
+
+    @Column(name = "emergency_contact", columnDefinition = "TEXT")
+    private String emergencyContact;
+
+    @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Encounter> encounters;
+
 }
--- src/main/java/model/operations/Encounter.java
+++ src/main/java/model/operations/Encounter.java
@@ -1,4 +1,41 @@
 package model.operations;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import model.AccessControl.Staff;
+import model.PatientManagement.Patient;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "encounters")
+@Getter
+@Setter
 public class Encounter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "encounter_id")
+    private Integer id;
+
+    @Column(name = "visit_type", length = 50)
+    private String visitType;
+
+    @Column( name = "start_time")
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
+
+    @Column(name = "status", length = 50)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id")
+    private Staff doctor;
 }
