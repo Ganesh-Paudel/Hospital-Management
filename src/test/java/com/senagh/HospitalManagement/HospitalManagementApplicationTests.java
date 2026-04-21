5f74a2fcb739b4da77059fb372948dfca12f5a32
Ganesh <paudelganesh080@gmail.com>
2026-04-21 02:15:20 -0400

    Initial repo

---
+++ src/test/java/com/senagh/HospitalManagement/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senagh.HospitalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
